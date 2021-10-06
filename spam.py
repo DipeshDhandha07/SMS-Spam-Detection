7cf4805705dca9b39a3f87b7d3c62f2a04e03b37
Dipesh Dhandha <55910147+DipeshDhandha07@users.noreply.github.com>
2021-10-06 14:21:23 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -51,8 +51,3 @@ if st.button('Predict'):
         st.header("Spam")
     else:
         st.header("Not Spam")
-        
-st.header("\n")
-st.header("\n")
-html_string = "<a href=https://sms-spam-login.herokuapp.com/>Logout</a>"
-st.markdown(html_string, unsafe_allow_html=True)
