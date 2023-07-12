dcfb1a9b7665195f755dbe8f95c2524267aa3c01
Dipesh Dhandha <55910147+DipeshDhandha07@users.noreply.github.com>
2023-07-12 10:42:57 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,60 @@
+import pyrebase
+import streamlit as st
+
+# Configuration Key
+firebaseConfig = {
+  'apiKey': "AIzaSyBipeCZHz76Wt95OxlNreUN42snxoapTvs",
+  'authDomain': "spam-detection-c83e2.firebaseapp.com",
+  'projectId': "spam-detection-c83e2",
+  'databaseURL': "https://spam-detection-c83e2-default-rtdb.europe-west1.firebasedatabase.app/",
+  'storageBucket': "spam-detection-c83e2.appspot.com",
+  'messagingSenderId': "652569329387",
+  'appId': "1:652569329387:web:738dbc6e337587af09c8ae",
+  'measurementId': "G-NRZM5Z40R5"
+}
+
+# Firebase authentication
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+# Database
+db = firebase.database()
+storage = firebase.storage()
+
+st.title("SMS Spam Detection")
+st.header("\n")
+st.image("SMS-Spam.jpg",width=500)
+st.sidebar.title("Login")
+
+# Authentication
+email = st.sidebar.text_input("Enter your email")
+password = st.sidebar.text_input("Enter your password",type="password")
+choice = st.sidebar.selectbox('Login/Signup',['Login','Sign up'])
+
+if choice == 'Sign up':
+  username = st.sidebar.text_input('Enter your username')
+  submit = st.sidebar.button("Create account")
+
+  if submit:
+    user = auth.create_user_with_email_and_password(email, password)
+    st.success('Your account is created successfully!')
+    st.balloons()
+
+    # Sign in
+    user = auth.sign_in_with_email_and_password(email, password)
+    db.child(user['localId']).child("Username").set(username)
+    db.child(user['localId']).child("Id").set(user['localId'])
+    st.title("Welcome "+username)
+    st.info("Login via the dropdown box")
+
+elif choice == 'Login':
+  sub = st.sidebar.button("Login")
+
+  if sub:
+      try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        st.success('Login Successfull!')
+        html_string = "<a href=https://sms-spam-detection-nextgen.herokuapp.com/>Open app</a>"
+        st.markdown(html_string, unsafe_allow_html=True)
+      except:
+        st.success("Invalid credentials")
